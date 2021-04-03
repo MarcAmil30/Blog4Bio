3480923b2844b70045d3610ccfd71ae8f125d9a2
Marc Amil <marc.amil18@imperial.ac.uk>
2021-04-03 05:35:40 +0200

    First commit

---
+++ scripts/main.js
@@ -0,0 +1,42 @@
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/image_1.jpeg') {
+      myImage.setAttribute('src','images/CRISPR.jpeg');
+    } else {
+      myImage.setAttribute('src','images/image_1.jpeg');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool, ' + myName;
+  }
+
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+  }
+
+  myButton.onclick = function() {
+    setUserName();
+  }
+
+  function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.textContent = 'Mozilla is cool, ' + myName;
+    }
+  }
+  
